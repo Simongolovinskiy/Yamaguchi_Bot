419486e9d8f7716c45e571a7653d208e5f229d09
wasa <tositrent164@gmail.com>
2023-12-30 16:41:58 +0300

    Reimplemented free massage

--- src/seleniumScript/http_actions.py
+++ src/seleniumScript/http_actions.py
@@ -1,4 +1,5 @@
 import time
+import json
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -16,37 +17,46 @@ class SeleniumBot:
         self.client = OnlineSimHandler()
         self.url = url
 
-    def number_page(self) -> None:
-        self.driver.get(self.url)
-        element = self.driver.find_element(By.XPATH, "//*[text()='Бесплатно 10 мин']")
-        element.click()
-        element = self.driver.find_element(By.LINK_TEXT, "Зарегистрироваться")
-        element.click()
-
-    def enter_registration_data(self):
-        name_input = self.driver.find_element(By.NAME, "name")
-        email_input = self.driver.find_element(By.NAME, "email")
-        phone_input = self.driver.find_element(By.NAME, "phone")
-
-        number = self.client.get_new_number()
+    def enter_email(self) -> None:
+        name_input = self.driver.find_element(By.ID, "name")
+        email_input = self.driver.find_element(By.ID, "email")
+        submit_button = self.driver.find_element(By.ID, "submit")
 
         name_input.send_keys(generate_random_name())
         email_input.send_keys(generate_random_email())
-        phone_input.send_keys(number)
-        element = self.driver.find_element(By.ID, "submit")
+        submit_button.click()
+        return
+
+    def enter_phone_number(self):
+        self.driver.get(self.url)
+        phone_input = self.driver.find_element(By.NAME, "phone")
+        phone_input.click()
+
+        number = self.client.get_new_number()
+        number_for_input = number[2:]
+
+        phone_input.send_keys(number_for_input)
+
+        element = self.driver.find_element(By.ID, "submitButton")
         element.click()
         return number
 
-    def receive_input_code(self, number):
-        time.sleep(5)
+    def enter_input_code(self, number):
+        time.sleep(7)
         msg_code = self.client.get_rented_number_message(number)
-        element = self.driver.find_element(By.NAME, "verificationCode")
-        element.send_keys(msg_code)
-        element = self.driver.find_element(By.NAME, "submit")
+
+        for elem in range(4):
+            element = self.driver.find_element(
+                    By.NAME,
+        f"form[verificationCodeDigits][{elem}]")
+            element.click()
+
+            element.send_keys(msg_code[elem])
+
+        element = self.driver.find_element(By.ID, "form-submit")
         element.click()
 
     def run(self):
-        self.number_page()
-        number = self.enter_registration_data()
-        self.receive_input_code(number)
-
+        number = self.enter_phone_number()
+        self.enter_input_code(number)
+        self.enter_email()
